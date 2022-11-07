514d6fcf20a3bba16ea8d136b992f0231ea26f97
unknown <nurul.islam96@aol.com>
2022-11-08 01:32:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+//MiddleWare
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@cluster0.yfy0tas.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+//DB collections
+const serviceCollection = client.db('dentisia').collection('services');
+
+//DB connect
+async function dbConnect (){
+    try {
+        await client.connect()
+        console.log('Database is Connected')
+    } catch (error) {
+        console.log(error)
+    }
+}
+dbConnect();
+
+//Server Connection Status
+app.get('/', (req,res)=>{
+    res.send('API is Running')
+})
+app.listen(port, ()=> console.log('Server is running through port: ', port))
