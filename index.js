721f3d5ba7f5b5948442cf7bf9743fff8d4676a8
unknown <nurul.islam96@aol.com>
2022-11-08 22:26:45 +0600

    added add service function

--- index.js
+++ index.js
@@ -1,45 +1,72 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 //MiddleWare
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@cluster0.yfy0tas.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 //DB connect
-async function dbConnect (){
-    try {
-        await client.connect()
-        console.log('Database is Connected')
-    } catch (error) {
-        console.log(error)
-    }
+async function dbConnect() {
+  try {
+    await client.connect();
+    console.log("Database is Connected");
+  } catch (error) {
+    console.log(error);
+  }
 }
 dbConnect();
 
 //DB collections
 const serviceCollection = client.db("dentisia").collection("services");
-console.log(serviceCollection);
 
-app.get('/home', async(req,res)=>{
-    try {
-        const cursor =  serviceCollection.find({})
-        const services = await cursor.limit(3).toArray()
-        res.send(services)
-    } catch (error) {
-        console.log(error.message)
-    }
-})
+//For Home Section
+app.get("/home", async (req, res) => {
+  try {
+    const cursor = serviceCollection.find({});
+    const services = await cursor.limit(3).toArray();
+    res.send(services);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+//For Services Section
+app.get("/services", async (req, res) => {
+  try {
+    const cursor = serviceCollection.find({});
+    const services = await cursor.toArray();
+    res.send(services);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+//Add Service
+app.post("/services", async (req, res) => {
+  try {
+    const query = req.body;
+    const result = await serviceCollection.insertOne(query)
+    console.log(result);
+    res.send(result)
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
 //Server Connection Status
-app.get('/', (req,res)=>{
-    res.send('API is Running')
-})
-app.listen(port, ()=> console.log('Server is running through port: ', port))
+app.get("/", (req, res) => {
+  res.send("API is Running");
+});
+app.listen(port, () => console.log("Server is running through port: ", port));
