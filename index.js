ecc8942354c02bb702da15eef53dd3bdbdd9c19c
unknown <nurul.islam96@aol.com>
2022-11-09 03:15:39 +0600

    added timestamp

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId, Timestamp } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -30,12 +30,13 @@ dbConnect();
 
 //DB collections
 const serviceCollection = client.db("dentisia").collection("services");
+const reviewsCollection = client.db("dentReviews").collection("reviews");
 
 //For Home Section
 app.get("/home", async (req, res) => {
   try {
     const cursor = serviceCollection.find({});
-    const services = await cursor.limit(3).toArray();
+    const services = await cursor.limit(3).sort({ _id: -1 }).toArray();
     res.send(services);
   } catch (error) {
     console.log(error.message);
@@ -46,24 +47,50 @@ app.get("/home", async (req, res) => {
 app.get("/services", async (req, res) => {
   try {
     const cursor = serviceCollection.find({});
-    const services = await cursor.toArray();
+    const services = await cursor.sort({ _id: -1 }).toArray();
     res.send(services);
   } catch (error) {
     console.log(error.message);
   }
 });
 
+app.get('/services/:id', async(req,res) => {
+    const {id} = req.params
+    const result = await serviceCollection.findOne({_id: ObjectId(id)})
+    res.send(result)
+})
+
 //Add Service
 app.post("/services", async (req, res) => {
   try {
     const query = req.body;
-    const result = await serviceCollection.insertOne(query)
+    const result = await serviceCollection.insertOne(query);
     console.log(result);
-    res.send(result)
+    res.send(result);
   } catch (error) {
     console.log(error.message);
   }
 });
+
+//Add Review
+app.post('/reviews/', async(req,res)=>{
+    try {
+        const query = req.body;
+        const {serviceId, email, username, photoURL, message} = query;
+        const result = await reviewsCollection.insertOne({serviceId, email, username, photoURL, message, "timestamp": new Date()})
+        res.send(result)
+    } catch (error) {
+        console.log(error.message)
+    }
+})
+
+//Get Review
+app.get('/reviews', async(req,res)=>{
+    const query = req.query.serviceId;
+    const cursor = await reviewsCollection.find({serviceId: query})
+    const result = await cursor.sort({timestamp: -1}).limit(3).toArray()
+    res.send(result)
+})
 
 //Server Connection Status
 app.get("/", (req, res) => {
